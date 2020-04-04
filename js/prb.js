ece4bb646b00f2f7b842a49cea54b6cc4b8f8aed
VinnieSanchez11 <it.coordinator.samsug@gmail.com>
2020-04-03 20:08:12 -0600

    penultima

--- js/prb.js
+++ js/prb.js
@@ -18,7 +18,10 @@ var Calculadora = {
     mas:document.getElementById('mas'),
     punto:document.getElementById('punto'),
     igual:document.getElementById('igual'),
+    raiz:document.getElementById('raiz'),
     teclas:document.getElementsByClassName('tecla'),
+    datoUno:"0",
+    tipoOperacion:"",
 
     eventos: function() {
         this.uno.addEventListener('click', function() {
@@ -61,22 +64,36 @@ var Calculadora = {
         this.on.addEventListener('click', function() {
           Calculadora.borrar();
         })
-
-        this.igual.addEventListener('click', function() {
+        this.raiz.addEventListener('click', function() {
           Calculadora.idtipo();
         })
+
         this.mas.addEventListener('click', function() {
-          Calculadora.mostrar(8);
+          Calculadora.guardarDatos('1');
         })
         this.menos.addEventListener('click', function() {
-          Calculadora.mostrar(9);
+          Calculadora.guardarDatos('2');
         })
         this.por.addEventListener('click', function() {
-          Calculadora.mostrar(0);
+          Calculadora.guardarDatos('3');
         })
         this.div.addEventListener('click', function() {
-          Calculadora.borrar();
+          Calculadora.guardarDatos('4');
         })
+        this.igual.addEventListener('click', function() {
+          Calculadora.operaciones();
+        })
+        for (var i = 0; i < this.teclas.length; i++){
+          this.teclas[i].addEventListener('mousedown', function () {
+            this.style.transform = "scale(0.9)";
+          } )
+        }
+        for (var i = 0; i < this.teclas.length; i++){
+          this.teclas[i].addEventListener('mouseup', function () {
+            this.style.transform = "scale(1)";
+          } )
+        }
+
     },
 
     mostrar:function(numero) {
@@ -115,12 +132,52 @@ var Calculadora = {
            this.display.innerHTML += "" ;
         }
    else if (this.display.innerHTML.indexOf("-") != -1){
-            //  this.display.innerHTML += "" ;
+
             this.display.innerHTML = this.display.innerHTML.slice(1,9);
            }
    else {
               this.display.innerHTML = numero  + this.display.innerHTML ;
             }
+
+    },
+
+    operaciones:function() {
+      var dato1 = Number(this.datoUno);
+      var datoDos = this.display.innerHTML;
+      var dato2 = Number(datoDos);
+      var nuevoResultado;
+      var resultado = Number(resultado);
+      opcion = this.tipoOperacion;
+      switch (opcion) {
+        case '1':
+          resultado = dato1 + dato2;
+          this.display.innerHTML = resultado;
+          this.display.innerHTML = this.display.innerHTML.slice(0,8);
+          break;
+        case '2':
+          resultado = dato1 - dato2
+          this.display.innerHTML = resultado;
+          this.display.innerHTML = this.display.innerHTML.slice(0,8);
+          break;
+        case '3':
+          resultado = dato1 * dato2
+
+          this.display.innerHTML = resultado;
+          this.display.innerHTML.slice(0,8);
+          break;
+        case '4':
+          resultado = dato1 / dato2
+          this.display.innerHTML = resultado;
+          this.display.innerHTML = this.display.innerHTML.slice(0,8);
+          break;
+      }
+
+    },
+
+    guardarDatos:function(ope) {
+      this.datoUno = this.display.innerHTML;
+      this.tipoOperacion = ope;
+      this.display.innerHTML = "";
 
     },
 
@@ -131,14 +188,6 @@ var Calculadora = {
         alert(typeof tipo);
 
 
-    },
-
-
-    mostrarTecla:function(){
-
     }
-
   }
-
-
   Calculadora.eventos();
